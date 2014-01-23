c7b6d50b8dfc238e1b113c7303a31f73057e00b3
brojec <brojec@msn.com>
2014-01-23 10:36:02 -0500

    cleaner code, proj02 args added

--- messenger465_client_template.py
+++ messenger465_client_template.py
@@ -29,54 +29,50 @@ class MessageBoardNetwork(object):
 	'''
 	#Carrie
 	def __init__(self, host, port):
-	'''        
-	Constructor.  You should create a new socket
-	here and do any other initialization.
-	CHECK!!
-	'''
-	self.host = host
-	self.port = port
-	self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		'''        
+		Constructor.  You should create a new socket
+		here and do any other initialization.
+		CHECK!!
+		'''
+		self.host = host
+		self.port = port
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.version = 'C'
+		self.timeout = 0.1
+		self.recv = 1400
 
 #Carrie & Brett
 	def getMessages(self):
-	'''
-	You should make calls to get messages from the message 
-	board server here.
-	'''
-	self.sock.sendto('AGET', (self.host, self.port))
-	time = select([self.sock], [], [], 0.1)
-	if time[0] == []:
-		raise socket.error('timeout');
-	else:
-		result = self.sock.recvfrom(1500)
-	str1 = result[0]
-	returnlist = str1.split('::') #break up by ::
-	str2 = returnlist[0].split(' ') #dividing up first element to see if first part of that is 'OK'
-	if(str2[0]!='AOK'):
-		raise socket.error(' '.join(str2))
-	returnlist[0] = returnlist[0][4:] #getting rid of 'AOK'
-	newlist = []
-	for i in range(0,len(returnlist),3):
-		newlist.append(' '.join(returnlist[i:i+3]))
-	return newlist
+		'''
+		You should make calls to get messages from the message 
+		board server here.
+		'''
+		self.sock.sendto('AGET', (self.host, self.port))
+		time = select([self.sock], [], [], self.timeout)
+		if time[0] == []:
+			raise socket.error('timeout');
+		else:
+			result = self.sock.recvfrom(self.recv)
+		str1 = result[0]
+		returnlist = str1.split('::') #break up by ::
+		str2 = returnlist[0].split(' ') #dividing up first element to see if first part of that is 'OK'
+		if(str2[0]!=self.version+'OK'):
+			raise socket.error(' '.join(str2))
+		returnlist[0] = returnlist[0][4:] #getting rid of 'XOK'
+		newlist = []
+		for i in range(0,len(returnlist),3):
+			newlist.append(' '.join(returnlist[i:i+3]))
+		return newlist
 
 #Brett
 	def postMessage(self, user, message):
-	if len(user)>8:
-		raise socket.error('username cannot be more than 8 characters')
-		if len(message)>60:
-			raise socket.error('message must be less than 60 characters')
-	if '::' in user or '::' in message:
-		raise socket.error('message, username cannot contain "::"')
-	self.sock.sendto('APOST ' + user + '::' + message, (self.host, self.port))
-	time = select([self.sock], [], [], 0.1)
-	if time[0] == []:
-		raise socket.error('timeout')
-	else:
-		resp = self.sock.recvfrom(1500)[0]
-	if len(resp)!=2:
-		raise socket.error(resp)
+		time = select([self.sock], [], [], self.timeout)
+		if time[0] == []:
+			raise socket.error('timeout')
+		else:
+			resp = self.sock.recvfrom(self.recv)[0]
+		if len(resp)!=2:
+			raise socket.error(resp)
 
 
 
@@ -88,21 +84,30 @@ class MessageBoardController(object):
 	'''
 
 	def __init__(self, myname, host, port):
-	self.name = myname
-	self.view = MessageBoardView(myname)
-	self.view.setMessageCallback(self.post_message_callback)
-	self.net = MessageBoardNetwork(host, port)
+		self.name = myname
+		self.view = MessageBoardView(myname)
+		self.view.setMessageCallback(self.post_message_callback)
+		self.net = MessageBoardNetwork(host, port)
 
 	def run(self):
-	self.view.after(1000, self.retrieve_messages)
-	self.view.mainloop()
+		self.view.after(1000, self.retrieve_messages)
+		self.view.mainloop()
 
-#Brett
+	#Brett
 	def post_message_callback(self, m):
-	try:
-		self.net.postMessage(myname, m)
-	except socket.error as err:
-		self.view.setStatus(err.message)
+		if(len(self.name)>8):
+			self.view.setStatus("User name >8 characters")
+			return;
+		elif(len(m)>60):
+			self.view.setStatus("Message >60 characters")
+			return;
+		elif('::' in self.name or '::' in m):
+			self.view.setStatus("message, username cannot contain '::'");
+			return;
+		try:
+			self.net.postMessage(self.name, m);
+		except socket.error as err:
+			self.view.setStatus(err.message);
 	
 
 	#Carrie
@@ -143,67 +148,67 @@ class MessageBoardView(Tkinter.Frame):
 	need to modify the below code.
 	'''
 	def __init__(self, name):
-	self.root = Tkinter.Tk()
-	Tkinter.Frame.__init__(self, self.root)
-	self.root.title('{} @ messenger465'.format(name))
-	self.width = 80
-	self.max_messages = 20
-	self._createWidgets()
-	self.pack()
+		self.root = Tkinter.Tk()
+		Tkinter.Frame.__init__(self, self.root)
+		self.root.title('{} @ messenger465'.format(name))
+		self.width = 80
+		self.max_messages = 20
+		self._createWidgets()
+		self.pack()
 
 	def _createWidgets(self):
-	self.message_list = Tkinter.Listbox(self, width=self.width, height=self.max_messages)
-	self.message_list.pack(anchor="n")
+		self.message_list = Tkinter.Listbox(self, width=self.width, height=self.max_messages)
+		self.message_list.pack(anchor="n")
 
-	self.entrystatus = Tkinter.Frame(self, width=self.width, height=2)
-	self.entrystatus.pack(anchor="s")
+		self.entrystatus = Tkinter.Frame(self, width=self.width, height=2)
+		self.entrystatus.pack(anchor="s")
 
-	self.entry = Tkinter.Entry(self.entrystatus, width=self.width)
-	self.entry.grid(row=0, column=1)
-	self.entry.bind('<KeyPress-Return>', self.newMessage)
+		self.entry = Tkinter.Entry(self.entrystatus, width=self.width)
+		self.entry.grid(row=0, column=1)
+		self.entry.bind('<KeyPress-Return>', self.newMessage)
 
-	self.status = Tkinter.Label(self.entrystatus, width=self.width, text="starting up")
-	self.status.grid(row=1, column=1)
+		self.status = Tkinter.Label(self.entrystatus, width=self.width, text="starting up")
+		self.status.grid(row=1, column=1)
 
-	self.quit = Tkinter.Button(self.entrystatus, text="Quit", command=self.quit)
-	self.quit.grid(row=1, column=0)
+		self.quit = Tkinter.Button(self.entrystatus, text="Quit", command=self.quit)
+		self.quit.grid(row=1, column=0)
 
 
 	def setMessageCallback(self, messagefn):
-	'''
-	Set up the callback function when a message is generated 
-	from the GUI.
-	'''
-	self.message_callback = messagefn
+		'''
+		Set up the callback function when a message is generated 
+		from the GUI.
+		'''
+		self.message_callback = messagefn
 
 	def setListItems(self, mlist):
-	'''
-	mlist is a list of messages (strings) to display in the
-	window.  This method simply replaces the list currently
-	drawn, with the given list.
-	'''
-	self.message_list.delete(0, self.message_list.size())
-	self.message_list.insert(0, *mlist)
+		'''
+		mlist is a list of messages (strings) to display in the
+		window.  This method simply replaces the list currently
+		drawn, with the given list.
+		'''
+		self.message_list.delete(0, self.message_list.size())
+		self.message_list.insert(0, *mlist)
 	
 	def newMessage(self, evt):
-	'''Called when user hits entry in message window.  Send message
-	to controller, and clear out the entry'''
-	message = self.entry.get()  
-	if len(message):
-		self.message_callback(message)
-	self.entry.delete(0, len(self.entry.get()))
+		'''Called when user hits entry in message window.  Send message
+		to controller, and clear out the entry'''
+		message = self.entry.get()  
+		if len(message):
+			self.message_callback(message)
+		self.entry.delete(0, len(self.entry.get()))
 
 	def setStatus(self, message):
-	'''Set the status message in the window'''
-	self.status['text'] = message
+		'''Set the status message in the window'''
+		self.status['text'] = message
 
 	def end(self):
-	'''Callback when window is being destroyed'''
-	self.root.mainloop()
-	try:
-		self.root.destroy()
-	except:
-		pass
+		'''Callback when window is being destroyed'''
+		self.root.mainloop()
+		try:
+			self.root.destroy()
+		except:
+			pass
 
 if __name__ == '__main__':
 
